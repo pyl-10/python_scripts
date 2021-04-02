a3925c6b8cf55bf412480fa5aba1e136f13867ce
OneZero <pw1654453533@gmail.com>
2021-04-02 11:35:30 +0800

    词频统计排序

--- word_count_sort.py
+++ word_count_sort.py
@@ -1,24 +1,37 @@
-def count_words(filename):
-    """计算一个文件大致包含多少单词"""
-    try:
-        with open(filename) as f_obj:
-            contents = f_obj.read()
-    except FileNotFoundError:
-        pass
-        # msg = '没找到文件'
-        # print(msg)
-    else:
-        words = contents.split()
-        new_words = len(words)
-        print(new_words)
+import re
 
-        count_word = 0
+def parse(text):
+    # 使用正则表达式去除标点符号和换行符
+    text = re.sub(r'[^\w ]', ' ', text)
 
-        for word in words:
-            count_word += word.lower().count('you')
+    # 转为小写
+    text = text.lower()
 
-        print(count_word)
+    # 生成所有单词的列表
+    word_list = text.split(' ')
 
-filenames = ['alice.txt','little_women.txt','moby_dict.txt','siddhartha.txt']
-for filename in filenames:
-    count_words(filename)
+    # 去除空白单词
+    word_list = filter(None, word_list)
+
+    # 生成单词和词频的字典
+    word_cnt = {}
+    for word in word_list:
+        if word not in word_cnt:
+            word_cnt[word] = 0
+        word_cnt[word] += 1
+
+    # 按照词频排序
+    sorted_word_cnt = sorted(
+        word_cnt.items(), key=lambda kv: kv[1], reverse=True)
+
+    return sorted_word_cnt
+
+
+with open('in.txt', 'r') as fin:
+    text = fin.read()
+
+word_and_freq = parse(text)
+
+with open('out.txt', 'w') as fout:
+    for word, freq in word_and_freq:
+        fout.write('{} {}\n'.format(word, freq))
