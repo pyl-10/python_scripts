0320d14280b57c7471eb9da049afe960ed40e7ea
OneZero <pw1654453533@gmail.com>
2021-04-02 11:25:52 +0800

    百度ocr识别测试

---
+++ text_baidu_ocr.py
@@ -0,0 +1,27 @@
+from aip import  AipOcr
+
+'''
+图片输入
+图片预处理
+文字检测
+文字识别
+输出
+'''
+
+# 你的APPID AK SK
+APP_ID = '23919599'
+API_KEY = 'GVG2dxUKVtUWZDHfCUkuSXdo'
+SECRET_KEY = '6Lzu5uheHgWf1DyfXVQ7LxwGXMUGBvSt'
+
+client = AipOcr(APP_ID, API_KEY, SECRET_KEY)
+# 读取图片
+def get_file_content(filePath):
+    with open(filePath, 'rb') as fp:
+        return fp.read()
+image = get_file_content('demo.jpg')
+# 调用通用文字识别， 图片参数为本地图片
+result = client.basicGeneral(image)
+info = []
+for i in result['words_result']:
+    info.append(i['words'])
+print(info)
